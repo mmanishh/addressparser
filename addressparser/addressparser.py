f529e5863ce0a0f30f89339bbc8fb7fe5bfb2550
mmanishh <dfrozenthrone@gmail.com>
2019-07-09 11:19:16 +0545

    intiated

---
+++ addressparser/addressparser.py
@@ -0,0 +1,24 @@
+import spacy
+import os
+
+class AddressParser:
+
+    def __init__(self,model_path="model/model_combined_9730"):
+        model_file_path = os.path.join(os.path.dirname(__file__), model_path)
+        self.nlp = spacy.load(model_file_path)
+    
+
+    def parse_address(self,addr):
+        model = self.nlp
+        doc = model(addr.lower())
+        result = {}
+        # Find named entities, phrases and concepts
+        for ent in doc.ents:
+            start, end = ent.start_char, ent.end_char
+            #print('start:{0},end:{1}'.format(start,end))
+            #print('text:',addr[start:end], 'label:',ent.label_)
+            result[ent.label_] = addr[start:end]
+
+        return result
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from addressparser import AddressParser
+
+if __name__ == "__main__":
+    print(AddressParser("model/model_combined_9730").parse_address('Cibadak Kec. Astanaanyar Kota Bandung, Jawa Barat 40241 Jl. Jend. Sudirman No.198'))
