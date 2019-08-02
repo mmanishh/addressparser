a8a97ff9b9cadb57d99283ad7607ba1799014574
manish <dfrozenthrone@gmail.com>
2019-08-02 12:43:24 +0545

    add change to str and None data type handling

--- addressparser/core/addressparser.py
+++ addressparser/core/addressparser.py
@@ -2,21 +2,24 @@ import spacy
 import os
 import pandas as pd
 
+
 class AddressParser:
 
-    def __init__(self,model_path="model/model_combined_9730"):
+    def __init__(self, model_path="model/model_combined_9730"):
         model_file_path = os.path.join(os.path.dirname(__file__), model_path)
         self.nlp = spacy.load(model_file_path)
-    
 
-    def parse_address(self,addr):
+    def parse_address(self, addr):
         """
         Function to parse single string address
         Params:
         addr = string address to parse
         Returns: parsed address in json format
         """
+        if addr is None:
+            return {'other':'null'}
         model = self.nlp
+        addr = str(addr)
         doc = model(str(addr).lower())
         result = {}
         # Find named entities, phrases and concepts
@@ -26,7 +29,7 @@ class AddressParser:
 
         return result
 
-    def parse_csv(self,file_path,cols,nrows=1000,delimiter=','):
+    def parse_csv(self, file_path, cols, nrows=1000):
         """
         function to parse whole csv file for the specified cols
         params:
@@ -35,18 +38,14 @@ class AddressParser:
         cols = list of column index/col name to parse  
         returns : Parsed DataFrame
         """
-        df = pd.read_csv(file_path,nrows=nrows,delimiter=delimiter, error_bad_lines = False)
-        
+        df = pd.read_csv(file_path, nrows=nrows, sep=';')
+
         for col in cols:
-            if isinstance(col,int):
-                new_col = colname = df.columns[col]+'_parsed'
-                df[new_col] = df.iloc[:,col].apply(self.parse_address)
+            if isinstance(col, int):
+                new_col = colname = df.columns[col] + '_parsed'
+                df[new_col] = df.iloc[:, col].apply(self.parse_address)
             else:
-                new_col = colname = col+'_parsed'
+                new_col = colname = col + '_parsed'
                 df[new_col] = df[col].apply(self.parse_address)
-           
 
         return df
-
-
-
