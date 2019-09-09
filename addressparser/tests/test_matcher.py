f0f35108b7d835f5b74e82dca66cdd5fcbb6eb2e
manish <dfrozenthrone@gmail.com>
2019-09-09 17:08:59 +0545

    change in testcase

--- addressparser/tests/test_matcher.py
+++ addressparser/tests/test_matcher.py
@@ -6,20 +6,22 @@ def test_get_params():
 
     query = {'city': 'depok', 'village': 'cimanggis', 'locality': 'rt'}
 
-    dummies = [{
-        "term": {
-            "city": "depok"
-        }
-    },
-        {
-            "term": {
-                "village": "cimanggis"
-            }
-        },
-        {
-            "term": {
-                "locality": "rt"
-            }
-        }]
+    # dummies = [{
+    #     "term": {
+    #         "city": "depok"
+    #     }
+    # },
+    #     {
+    #         "term": {
+    #             "village": "cimanggis"
+    #         }
+    #     },
+    #     {
+    #         "term": {
+    #             "locality": "rt"
+    #         }
+    #     }]
+
+    dummies = [{'match': {'city': 'depok'}}, {'match': {'village': 'cimanggis'}}, {'match': {'locality': 'rt'}}]
 
     assert dummies == es.generate_params(query)
